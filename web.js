9cd6d8ed527b03dfda46a4621c5320c72bf91124
John Smith <example@stanford.edu>
2013-07-10 16:20:38 +0000

    added web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 
 var express = require('express');
-
+var buff = (fs.readFileSync('index.html'));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffa = (fs.readFileSync('index.html'));
-    response.send(buffa.toString());
+//    var buffa = (fs.readFileSync('index.html'));
+    response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
